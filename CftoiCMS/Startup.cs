f70a3c756852f4abc09be61c58c320d7113f24c0
binhtruongit <binhtruong.it@gmail.com>
2017-10-05 14:00:29 +0700

    Init CftoiCMS

---
+++ CftoiCMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CftoiCMS.Startup))]
+namespace CftoiCMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
